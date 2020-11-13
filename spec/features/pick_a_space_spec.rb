f7da6fd61b4f10895c934cbb0f7fd4c4df31f227
Shamarni <Smcfarlane926@gmail.com>
2020-11-13 14:00:18 +0000

    added css in spaces.erb

--- spec/features/pick_a_space_spec.rb
+++ spec/features/pick_a_space_spec.rb
@@ -9,4 +9,15 @@ feature 'pick a space from the list' do
     click_button('Pick')
     expect(page).to have_content "Pick a date"
   end
+
+  scenario 'user picks a date on the calendar and clicks request a date button' do
+    add_row_to_test_database()
+    visit('/spaces')
+    click_button('Pick')
+    expect(current_path).to eq('/spaces/pick_a_date')
+
+    click_link("01")
+    click_button('request to book')
+    expect(page).to have_content('Requests')
+  end
 end
